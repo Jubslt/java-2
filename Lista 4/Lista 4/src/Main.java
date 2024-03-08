73dc036f53145f4ccd6a3137e129f1574c50ec31
Jubslt <julia.torres21@hotmail.com>
2024-03-07 22:36:41 -0300

    Estrutura de repetição

---
+++ Lista 4/Lista 4/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i=0;
+
+        while(i<=100){
+            int resto;
+            if(i%2==0){
+                resto = i;
+                System.out.println(resto);
+            }
+            i++;
+
+        }
+    }
+}
